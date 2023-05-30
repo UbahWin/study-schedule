5770942dd6af184d5bcdb3278f06e7098d9170ee
Ivan Vdovin <ivan03_vdovin@mail.ru>
2023-05-30 18:51:36 +0300

    Initial Commit

---
+++ StudySchedule/StudyScheduleApp.swift
@@ -0,0 +1,17 @@
+//
+//  StudyScheduleApp.swift
+//  StudySchedule
+//
+//  Created by Иван Вдовин on 30.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct StudyScheduleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
